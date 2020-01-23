85f3c8cc6f4758bbaa3b6ff9a82b43fa247a1d14
Manpreet Singh <manpreet.singh@cineplex.com>
2020-01-23 15:16:37 -0500

    attaching the view with activity and handling action

--- app/src/main/java/com/manpreet/timefighter/MainActivity.kt
+++ app/src/main/java/com/manpreet/timefighter/MainActivity.kt
@@ -2,11 +2,35 @@ package com.manpreet.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal var score = 0
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var timeLeft: TextView
+    internal lateinit var scoreText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tapMeButton = findViewById(R.id.tapMeButton)
+        scoreText = findViewById(R.id.score)
+        timeLeft = findViewById(R.id.timeLeft)
+
+        print("Hello")
+
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+    }
+
+    private fun incrementScore() {
+        score += 1
+        print(score)
+        val newScore = getString(R.string.scoreText, score)
+        scoreText.text = newScore
     }
 }
